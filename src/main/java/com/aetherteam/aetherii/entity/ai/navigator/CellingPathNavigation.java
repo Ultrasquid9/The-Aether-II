f70a3787c50db55e5c9e9f08ff274d31e4bb18db
baguchan <shutux500@gmail.com>
2024-11-20 16:45:31 +0900

    fixed celling monster path fix

--- src/main/java/com/aetherteam/aetherii/entity/ai/navigator/CellingPathNavigation.java
+++ src/main/java/com/aetherteam/aetherii/entity/ai/navigator/CellingPathNavigation.java
@@ -3,6 +3,8 @@ package com.aetherteam.aetherii.entity.ai.navigator;
 import com.aetherteam.aetherii.entity.ai.navigator.node.CellingNodeEvaluator;
 import com.aetherteam.aetherii.entity.monster.CellingMonster;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.protocol.game.DebugPackets;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
@@ -22,6 +24,12 @@ public class CellingPathNavigation extends GroundPathNavigation {
     @Override
     protected boolean canUpdatePath() {
         return true;
+    }
+
+
+    @Override
+    public boolean canCutCorner(PathType p_326808_) {
+        return this.cellingMonster.getAttachFacing() == Direction.DOWN && super.canCutCorner(p_326808_);
     }
 
     @Override
@@ -59,6 +67,57 @@ public class CellingPathNavigation extends GroundPathNavigation {
     }
 
     @Override
+    protected void followThePath() {
+        Vec3 vec3 = this.getTempMobPos();
+        this.maxDistanceToWaypoint = this.mob.getBbWidth() > 0.75F ? this.mob.getBbWidth() / 2.0F : 0.75F - this.mob.getBbWidth() / 2.0F;
+        Vec3i vec3i = this.path.getNextNodePos();
+
+        Direction reverseDirection = this.cellingMonster.getAttachFacing().getOpposite();
+        double widthOffset = this.cellingMonster.getAttachFacing() != Direction.DOWN ? 0.1F : 0.0F;
+
+        double d0 = Math.abs(this.mob.getX() - ((double) vec3i.getX() - (widthOffset * reverseDirection.getStepX()) + ((this.mob.getBbWidth() + 1) / 2D))); //Forge: Fix MC-94054
+        double d1 = Math.abs(this.mob.getY() - (double) vec3i.getY()) - (widthOffset * reverseDirection.getStepY());
+        double d2 = Math.abs(this.mob.getZ() - ((double) vec3i.getZ() - (widthOffset * reverseDirection.getStepZ()) + ((this.mob.getBbWidth() + 1) / 2D))); //Forge: Fix MC-94054
+
+        //no cut out
+        float fallDistance = this.mob.getMaxFallDistance();
+        boolean flag = d0 <= (double) this.maxDistanceToWaypoint && d2 <= (double) this.maxDistanceToWaypoint && d1 < 1;
+        if (flag || this.canCutCorner(this.path.getNextNode().type) && this.shouldTargetNextNodeInDirection(vec3)) {
+            this.path.advance();
+        }
+
+        this.doStuckDetection(vec3);
+    }
+
+    private boolean shouldTargetNextNodeInDirection(Vec3 pVec) {
+        if (this.path.getNextNodeIndex() + 1 >= this.path.getNodeCount()) {
+            return false;
+        } else {
+            Vec3 vec3 = Vec3.atBottomCenterOf(this.path.getNextNodePos());
+            if (!pVec.closerThan(vec3, 2.0)) {
+                return false;
+            } else if (this.canMoveDirectly(pVec, this.path.getNextEntityPos(this.mob))) {
+                return true;
+            } else {
+                Vec3 vec31 = Vec3.atBottomCenterOf(this.path.getNodePos(this.path.getNextNodeIndex() + 1));
+                Vec3 vec32 = vec3.subtract(pVec);
+                Vec3 vec33 = vec31.subtract(pVec);
+                double d0 = vec32.lengthSqr();
+                double d1 = vec33.lengthSqr();
+                boolean flag = d1 < d0;
+                boolean flag1 = d0 < 0.5;
+                if (!flag && !flag1) {
+                    return false;
+                } else {
+                    Vec3 vec34 = vec32.normalize();
+                    Vec3 vec35 = vec33.normalize();
+                    return vec35.dot(vec34) < 0.0;
+                }
+            }
+        }
+    }
+
+    @Override
     protected boolean canMoveDirectly(Vec3 p_186138_, Vec3 p_186139_) {
         return false;
     }
@@ -76,5 +135,14 @@ public class CellingPathNavigation extends GroundPathNavigation {
     @Override
     protected boolean hasValidPathType(PathType p_326937_) {
         return super.hasValidPathType(p_326937_) || p_326937_ == PathType.OPEN;
+    }
+
+    @Override
+    protected Vec3 getTempMobPos() {
+        if (this.cellingMonster.getAttachFacing() != Direction.DOWN) {
+            return new Vec3(this.mob.getX(), (double) this.mob.getY(), this.mob.getZ());
+        }
+
+        return super.getTempMobPos();
     }
 }
