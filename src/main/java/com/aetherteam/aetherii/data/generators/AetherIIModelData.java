3bc6a4466528753457d967424e75126274567f72
bconlon <bconlon.gml@gmail.com>
2025-01-03 13:44:30 -0800

    refactor: clean up AetherIIModelData

--- src/main/java/com/aetherteam/aetherii/data/generators/AetherIIModelData.java
+++ src/main/java/com/aetherteam/aetherii/data/generators/AetherIIModelData.java
@@ -5,10 +5,11 @@ import com.aetherteam.aetherii.data.generators.models.AetherIIBlockModels;
 import com.aetherteam.aetherii.data.generators.models.AetherIIItemModels;
 import net.minecraft.client.data.models.ModelProvider;
 import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LiquidBlock;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
@@ -25,31 +26,19 @@ public class AetherIIModelData extends ModelProvider {
         this.modelPathProvider = packOutput.createPathProvider(PackOutput.Target.RESOURCE_PACK, "models");
     }
 
+    @Override
     public CompletableFuture<?> run(CachedOutput output) {
-        ItemInfoCollector itemModelOutput = new ItemInfoCollector(this::getKnownItems) {
-            @Override
-            public void finalizeAndValidate() { //todo temporary
-                try {
-                    super.finalizeAndValidate();
-                } catch (IllegalStateException ignore) {
-
-                }
-            }
-        };
-        BlockStateGeneratorCollector blockModelOutput = new BlockStateGeneratorCollector(this::getKnownBlocks) {
-            @Override
-            public void validate() { //todo temporary
-                try {
-                    super.validate();
-                } catch (IllegalStateException ignore) {
-
-                }
-            }
-        };
+        ItemInfoCollector itemModelOutput = new ItemInfoCollector(this::getKnownItems);
+        BlockStateGeneratorCollector blockModelOutput = new BlockStateGeneratorCollector(this::getKnownBlocks);
         SimpleModelCollector modelOutput = new SimpleModelCollector();
         this.registerModels(new AetherIIBlockModels(blockModelOutput, itemModelOutput, modelOutput), new AetherIIItemModels(itemModelOutput, modelOutput));
         blockModelOutput.validate();
         itemModelOutput.finalizeAndValidate();
         return CompletableFuture.allOf(blockModelOutput.save(output, this.blockStatePathProvider), modelOutput.save(output, this.modelPathProvider), itemModelOutput.save(output, this.itemInfoPathProvider));
+    }
+
+    @Override
+    protected Stream<? extends Holder<Block>> getKnownBlocks() {
+        return BuiltInRegistries.BLOCK.listElements().filter((holder) -> holder.getKey().location().getNamespace().equals(this.modId) && !(holder.value() instanceof LiquidBlock));
     }
 }
